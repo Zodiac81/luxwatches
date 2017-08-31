014c5ba996b50c7bc87573348767bf8585b08c65
aleksey <aleksey@1000geeks.com>
2017-08-31 08:23:16 +0300

    no logo

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,8 +1,4 @@
 @extends('admin.layout.admin')
 @section('content')
-    <div class="logo">
-        <a href="{{ route('home') }}"><img src="{{asset('assets/images/logo_265x145.png')}}" alt="logoImg"></a>
-    </div>
-    		
-    	
+
 @endsection
